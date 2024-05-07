4add7d93b17a9baef6eba519d722c20d27329fb7
IsrAhmad <Israa.099@gmail.com>
2024-05-07 13:41:43 +0300

    header done

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HeaderService } from '../shared/header/services/header.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent {
+  userName: string ='';
+  name: string = '';
+  listData: any;
+  constructor(private _HeaderService:HeaderService) { }
 
+  ngOnInit() {
+    this.getUserName();
+  }
+
+  getUserName() {
+    this._HeaderService
+      .getUserName(this.userName, this.name)
+      .subscribe({
+        next: (res) => {
+          this.userName = res.userName;
+          this.name = res.name;
+          console.log(res);
+          this.listData = res;
+        },
+      });
+  }
 }
